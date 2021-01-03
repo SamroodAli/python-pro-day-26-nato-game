2d5571fcf12ac3775563cd838ec8cb2e3b3a1c2c
SamroodAli <samroodali@live.com>
2021-01-03 14:48:37 +0530

    Add:nato in list form

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ csv_file_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dictionary = {
     row.letter: row.code for (index, row) in csv_file_data.iterrows()
 }
+
+# saving the user input
+game_is_on = True
+while game_is_on:
+    user_word = input("Enter the word: ")
+    if user_word.lower() == "exit":
+        game_is_on = False
+        break
+
+    # user nato in dictionary
+    user_word_nato = {
+        letter: nato_dictionary[letter.title()] for letter in user_word
+    }
+    print("\nHere is your NATO code\n", user_word_nato)
+    # user nato in list form
+    user_word_nato_list = [code for (letter, code) in user_word_nato.items()]
+    print("\n", user_word_nato_list)
